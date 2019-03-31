80e3d897f2ca439601e9378912f76d5f77c68d32
Jack Phoenix <ashley@uncyclomedia.co>
2019-03-31 17:33:01 +0300

    Add phan config
    Change-Id: Ic5ec927de62c3bc796b20c9c2fcb789bb97b08dd

---
+++ .phan/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/SocialProfile',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		// We don't actually *depend on* SocialProfile, we merely *support* it, but phan cannot tell the difference.
+		'../../extensions/SocialProfile',
+	]
+);
+
+return $cfg;

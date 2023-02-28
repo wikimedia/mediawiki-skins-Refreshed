6253a28ce3476c4989ba4f8d7e674a0c5bbeab3d
Jack Phoenix <ashley@uncyclomedia.co>
2023-02-28 15:57:19 +0200

    Immediately delete cache keys if either of the relevant two Refreshed-specific interface messages gets deleted
    Previous cache invalidation code invalidated cache when either [[MediaWiki:Refreshed-navigation]] or [[MediaWiki:Refreshed-wiki-dropdown]] was edited.
    However, nothing was handling page deletion, so if the messages got deleted, cached data would still be displayed for up to 24 hours.
    
    Change-Id: I5a7823b864afebd43500d0707ff77e25ff8a0a1e

--- includes/SkinRefreshed.php
+++ includes/SkinRefreshed.php
@@ -12,15 +12,52 @@ class SkinRefreshed extends SkinTemplate {
 	 * @see https://phabricator.wikimedia.org/T166943
 	 */
 	public static function onPageSaveComplete( WikiPage $wikiPage ) {
+		$title = $wikiPage->getTitle();
+		self::purgeCacheKeys( $title );
+	}
+
+	/**
+	 * Like above, but for when either [[MediaWiki:Refreshed-wiki-dropdown]]
+	 * or [[MediaWiki:Refreshed-navigation]] was deleted...
+	 *
+	 * @param MediaWiki\Page\ProperPageIdentity $page The only param we care about
+	 * @param MediaWiki\Permissions\Authority $deleter
+	 * @param string $reason Deletion reason
+	 * @param int $pageID
+	 * @param MediaWiki\Revision\RevisionRecord $deletedRev
+	 * @param ManualLogEntry $logEntry
+	 * @param int $archivedRevisionCount
+	 */
+	public static function onPageDeleteComplete(
+		MediaWiki\Page\ProperPageIdentity $page,
+		MediaWiki\Permissions\Authority $deleter,
+		string $reason,
+		int $pageID,
+		MediaWiki\Revision\RevisionRecord $deletedRev,
+		ManualLogEntry $logEntry,
+		int $archivedRevisionCount
+	) {
+		$title = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $page )->getTitle();
+		self::purgeCacheKeys( $title );
+	}
+
+	/**
+	 * Given a Title (from the PageSaveComplete hook subscriber) or a MediaWiki\Page\ProperPageIdentity
+	 * (from the PageDeleteComplete hook subscriber), checks if either [[MediaWiki:Refreshed-wiki-dropdown]]
+	 * or [[MediaWiki:Refreshed-navigation]] was impacted (either edited or deleted), deletes
+	 * cached data.
+	 *
+	 * @param Title|MediaWiki\Page\ProperPageIdentity $title
+	 */
+	public static function purgeCacheKeys( $title ) {
 		global $wgLang;
 
 		$services = MediaWikiServices::getInstance();
 		$contLang = $services->getContentLanguage();
 		if ( $wgLang->getCode() != $contLang->getCode() ) {
-			return true;
+			return;
 		}
 
-		$title = $wikiPage->getTitle();
 		if ( $title->inNamespace( NS_MEDIAWIKI ) ) {
 			$cache = $services->getMainWANObjectCache();
 			$pageName = $title->getText();
